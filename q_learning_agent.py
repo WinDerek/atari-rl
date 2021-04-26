6338bbc3f0e99cc3b5a8a720e7ee04d08dd95797
derek <frankderekdick@gmail.com>
2021-04-26 17:38:13 +0800

    Adds some comments.

--- q_learning_agent.py
+++ q_learning_agent.py
@@ -20,7 +20,7 @@ num_episodes = 4000
 discount_factor = 0.8
 learning_rate = 0.9
 report_interval = 500
-report = '100-ep Average: %.2f . Best 100-ep Average: %.2f . Average: %.2f (Episode %d)'
+report = '100-ep Average: %.2f . Best 100-ep Average: %.2f . Total Average: %.2f (Episode %d)'
 
 
 def print_report(reward_list: List, episode: int):
@@ -37,26 +37,47 @@ def print_report(reward_list: List, episode: int):
 
 
 def main():
-    # Create the environment
+    # 创建环境
     env = gym.make('FrozenLake-v0')
 
-    # Make results reproducible
+    # 设置随机种子以使实验结果可复现
     env.seed(0)
 
     reward_list = []
 
-    Q = np.zeros((env.observation_space.n, env.action_space.n))
+    # 创建一个初始值全零的 Q 表格
+    # Q 表格用一个 numpy 二维数组表示
+    # 行数为状态空间的大小（即环境的观测空间的大小），列数为动作空间的大小
+    q_2darray = np.zeros((env.observation_space.n, env.action_space.n))
+
+    # 训练，每一个循环为一个 episode
     for episode in range(1, num_episodes + 1):
+        # 每个 episode 刚开始的时候，重置环境
         state = env.reset()
+
         episode_reward = 0
+
+        # 循环直至一个 episode 结束
+        # 每一轮循环 agent 会执行一次 action，即一个 step
         while True:
+            # 给动作加入噪声来增加 agent 的探索程度
             noise = np.random.random((1, env.action_space.n)) / (episode**2.)
-            action = np.argmax(Q[state, :] + noise)
-            state2, reward, done, _ = env.step(action)
-            Qtarget = reward + discount_factor * np.max(Q[state2, :])
-            Q[state, action] = (1-learning_rate) * Q[state, action] + learning_rate * Qtarget
+            action = np.argmax(q_2darray[state, :] + noise)
+
+            # 让 agent 在环境中执行动作
+            # state_to: 执行之后 agent 所处的状态
+            # reward: agent 获得的立即回报
+            # done: 表示当前 episode 是否结束的指示变量（bool）
+            state_to, reward, done, _ = env.step(action)
+            
+            # 更新 Q 表格
+            q_target = reward + discount_factor * np.max(q_2darray[state_to, :])
+            q_2darray[state, action] = (1 - learning_rate) * q_2darray[state, action] + learning_rate * q_target
+            
             episode_reward += reward
-            state = state2
+            state = state_to
+            
+            # 如果当前 episode 已经结束
             if done:
                 reward_list.append(episode_reward)
 
