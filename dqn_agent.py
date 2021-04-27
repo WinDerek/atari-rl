d15756edf467edfd79ccb715a08f4177441b8024
derek <frankderekdick@gmail.com>
2021-04-27 09:48:03 +0800

    Adds some comments.

--- dqn_agent.py
+++ dqn_agent.py
@@ -22,20 +22,29 @@ def downsample(state):
 
 
 def main():
+    # 解析命令行参数
     parser = argparse.ArgumentParser(description='Run DQN agent.')
     parser.add_argument('--model', type=str, help='Path to model', default='models/SpaceInvaders-v0.tfmodel')
     parser.add_argument('--visual', action='store_true')
     args = parser.parse_args()
 
-    # Create the environment
+    # 创建环境
     env = gym.make('SpaceInvaders-v0')
     
-    rewards = []
+    # 回报列表
+    reward_list = []
 
+    # 构建用于训练的模型，此处是一个 A3C 模型
     model = a3c_model(load=args.model)
+
+    # 训练循环，每一次迭代就是一个 episode
     for _ in range(num_episodes):
+        # 每个 episode 刚开始的时候，重置环境和相关数据
         episode_reward = 0
         states = [downsample(env.reset())]
+
+        # 循环直至一个 episode 结束
+        # 每一轮循环 agent 会执行一次 action，即一个 step
         while True:
             if len(states) < 4:
                 action = env.action_space.sample()
@@ -43,18 +52,26 @@ def main():
                 frames = np.concatenate(states[-4:], axis=3)
                 action = np.argmax(model([frames]))
 
+            # 如果需要 GUI 可视化
             if args.visual:
                 env.render()
             
+            # 让 agent 在环境中执行动作
+            # state: 执行之后 agent 所处的状态
+            # reward: agent 获得的立即回报
+            # done: 表示当前 episode 是否结束的指示变量（bool）
             state, reward, done, _ = env.step(action)
+
+            # 记录数据
             states.append(downsample(state))
             episode_reward += reward
             
+            # 如果当前 episode 已经结束
             if done:
                 print('Reward: %d' % episode_reward)
-                rewards.append(episode_reward)
+                reward_list.append(episode_reward)
                 break
-    print('Average reward: %.2f' % (sum(rewards) / len(rewards)))
+    print('Average reward: %.2f' % (sum(reward_list) / len(reward_list)))
 
 
 if __name__ == '__main__':
